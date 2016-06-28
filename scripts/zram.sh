57127f54a6f7b2790302e71a181cdd5802c96c58
Quyen <quyenach1234@gmail.com>
2016-06-28 19:13:54 +0700

    core33g: Import zram script, enable zram devices equal to CPU cores
    To prevent performance bottleneck

---
+++ scripts/zram.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+a=`getprop zram.disksize 64`
+b=`getprop sys.vm.swappiness 100`
+for i in `ls /sys/block/ | grep zram`; do
+echo $(($a*1024*1024)) > /sys/block/${i}/disksize;
+mkswap /dev/block/${i};
+swapon -p 05 /dev/block/${i};
+done
+echo $(($b)) > /proc/sys/vm/swappiness
+
+
+
