442baab5f8159aa9693b1016dc66a8ee993fe843
Quyen <quyenach1234@gmail.com>
2016-10-10 18:03:12 +0700

    Revert "BoardConfig: Dexopt boot image only"
    This reverts commit 707245bea236ecc444a1184dde2e45f03b815f10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,6 @@ PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
 
 # Enable dex-preoptimization to speed up the first boot sequence
 WITH_DEXPREOPT := true
-WITH_DEXPREOPT_BOOT_IMG_ONLY := true
 WITH_DEXPREOPT_PIC := true
 WITH_DEXPREOPT_COMP := false
 
