d2c8ece4430066d80d25686e7bbf56bdec13fb9c
Quyen <quyenach1234@gmail.com>
2016-10-16 11:58:56 +0700

    scripts: Fix a deadly derp

--- scripts/zram.sh
+++ scripts/zram.sh
@@ -6,9 +6,9 @@ mem=$(((totalmem / ${num_cores}) * 1024))
 
 for i in $(seq ${num_cores}); do
 DEVNUMBER=$((i - 1))
-echo ${mem} > /sys/block/${DEVNUMBER}/disksize;
-mkswap /dev/block/${DEVNUMBER};
-swapon /dev/block/${DEVNUMBER};
+echo ${mem} > /sys/block/zram${DEVNUMBER}/disksize;
+mkswap /dev/block/zram${DEVNUMBER};
+swapon /dev/block/zram${DEVNUMBER};
 done
 
 echo 100 > /proc/sys/vm/swappiness
