2697f785a6acea585aba14c2ecff41d189b3b56d
Quyen <quyenach1234@gmail.com>
2016-10-20 07:36:22 +0700

    scripts: Fixes zRAM script

--- scripts/zram.sh
+++ scripts/zram.sh
@@ -1,14 +1,9 @@
 #!/system/bin/sh
 
-num_cores=`grep processor /proc/cpuinfo | wc -l`
-totalmem=`free | grep -e "^Mem:" | sed -e 's/^Mem: *//' -e 's/  *.*//'`
-mem=$(((totalmem / ${num_cores}) * 1024))
-
-for i in $(seq ${num_cores}); do
-DEVNUMBER=$((i - 1))
-echo ${mem} > /sys/block/zram${DEVNUMBER}/disksize;
-mkswap /dev/block/zram${DEVNUMBER};
-swapon /dev/block/zram${DEVNUMBER};
+for i in 0 1 2 3; do
+echo 257M > /sys/block/zram${i}/disksize;
+mkswap /dev/block/zram${i};
+swapon /dev/block/zram${i};
 done
 
 echo 100 > /proc/sys/vm/swappiness
