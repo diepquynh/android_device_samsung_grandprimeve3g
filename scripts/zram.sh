ef78a1e7dc8747cfb5d2d88d64ae051e9ef25bb8
Quyen <quyenach1234@gmail.com>
2016-10-20 08:23:37 +0700

    scripts: zram.sh: Uses all zRAMs at the same time

--- scripts/zram.sh
+++ scripts/zram.sh
@@ -3,7 +3,7 @@
 for i in 0 1 2 3; do
 echo 257M > /sys/block/zram${i}/disksize;
 mkswap /dev/block/zram${i};
-swapon /dev/block/zram${i};
+swapon -p 05 /dev/block/zram${i};
 done
 
 echo 100 > /proc/sys/vm/swappiness
