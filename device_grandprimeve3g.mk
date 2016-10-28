3a154080cc67a7a50e39fa67fe43b4da006a6548
Quyen <quyenach1234@gmail.com>
2016-10-28 16:25:40 +0700

    device: Add SamsungDoze package, sensors permissions

--- device_grandprimeve3g.mk
+++ device_grandprimeve3g.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 	libgps_shim \
 	libstagefright_shim
 
+# Doze
+PRODUCT_PACKAGES += \
+	SamsungDoze
+
 # GPS
 GPS_CONFIGS := \
 	$(LOCAL_PATH)/configs/gps/gps.xml \
@@ -138,7 +142,11 @@ PRODUCT_COPY_FILES += \
 PERMISSIONS_XML_FILES := \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml \
-	frameworks/native/data/etc/android.hardware.camera.xml
+	frameworks/native/data/etc/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.software.midi.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml
 
 PRODUCT_COPY_FILES += \
 	$(foreach f,$(PERMISSIONS_XML_FILES),$(f):system/etc/permissions/$(notdir $(f)))
