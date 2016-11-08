6d023d4d506a7f47b409b1785ff1cd14010ecf03
Quyen <quyenach1234@gmail.com>
2016-11-08 11:53:37 +0700

    device: Add Gello, Snap camera

--- device_grandprimeve3g.mk
+++ device_grandprimeve3g.mk
@@ -100,9 +100,11 @@ PRODUCT_PACKAGES += \
 	libhwc_shim \
 	libstagefright_shim
 
-# Doze
+# Common packages
 PRODUCT_PACKAGES += \
-	SamsungDoze
+	SamsungDoze \
+	Snap \
+	Gello
 
 # GPS
 GPS_CONFIGS := \
@@ -150,6 +152,7 @@ PERMISSIONS_XML_FILES := \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.software.midi.xml \
