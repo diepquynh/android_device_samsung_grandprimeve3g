e4f5ce88955d70d403f21e408b7917cd4e491b5f
Juan Cortés <fam.cortesgutierrez@hotmail.com>
2017-01-06 03:46:27 -0300

    Repaired wrong lines (Sorry)

--- patch.sh
+++ patch.sh
@@ -2,11 +2,11 @@
 LOCALDIR = $(pwd)
 
 # Copy the patches
-cp $LOCALDIR/patches/build.patch		../../../build
-cp $LOCALDIR/patches/external_tinyalsa.patch	../../../external/tinyalsa
-cp $LOCALDIR/patches/frameworks_av.patch	../../../frameworks/av
-cp $LOCALDIR/patches/frameworks_base.patch	../../../frameworks/base
-cp $LOCALDIR/patches/system_core.patch		../../../system/core
+cp $LOCALDIR/patches/build.patch ../../../build
+cp $LOCALDIR/patches/external_tinyalsa.patch ../../../external/tinyalsa
+cp $LOCALDIR/patches/frameworks_av.patch ../../../frameworks/av
+cp $LOCALDIR/patches/frameworks_base.patch ../../../frameworks/base
+cp $LOCALDIR/patches/system_core.patch ../../../system/core
 
 # Apply them
 cd ../../..
@@ -16,7 +16,6 @@ cd ../../frameworks/av && patch -p1 < *.patch
 cd ../../frameworks/base && patch -p1 < *.patch
 cd ../../system/core && patch -p1 < *.patch
 
-#Move XML to .repo folder and sync
+#Move XML to .repo folder
 
 cp $LOCALDIR/XML/grandprimeve3g.xml ../../../.repo/manifests
-repo sync
