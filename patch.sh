54a3a12fe096a90f017591858319e7fcc9e7eadd
Juan <fam.cortesgutierrez@hotmail.com>
2017-01-06 03:42:23 -0300

    Added automatic set up

---
+++ patch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+LOCALDIR = $(pwd)
+
+# Copy the patches
+cp $LOCALDIR/patches/build.patch		../../../build
+cp $LOCALDIR/patches/external_tinyalsa.patch	../../../external/tinyalsa
+cp $LOCALDIR/patches/frameworks_av.patch	../../../frameworks/av
+cp $LOCALDIR/patches/frameworks_base.patch	../../../frameworks/base
+cp $LOCALDIR/patches/system_core.patch		../../../system/core
+
+# Apply them
+cd ../../..
+cd build && patch -p1 < *.patch
+cd ../external/tinyalsa && patch -p1 < *.patch
+cd ../../frameworks/av && patch -p1 < *.patch
+cd ../../frameworks/base && patch -p1 < *.patch
+cd ../../system/core && patch -p1 < *.patch
+
+#Move XML to .repo folder and sync
+
+cp $LOCALDIR/XML/grandprimeve3g.xml ../../../.repo/manifests
+repo sync
