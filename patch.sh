e87ba4724cb726b4823784f5760bf14e489764cd
Remilia <remilia.1505@gmail.com>
2017-01-06 13:51:50 +0700

    patch.sh: Add missing return path

--- patch.sh
+++ patch.sh
@@ -2,14 +2,14 @@
 LOCALDIR = $(pwd)
 
 # Copy the patches
-cp $LOCALDIR/patches/build.patch ../../../build
-cp $LOCALDIR/patches/external_tinyalsa.patch ../../../external/tinyalsa
-cp $LOCALDIR/patches/frameworks_av.patch ../../../frameworks/av
-cp $LOCALDIR/patches/frameworks_base.patch ../../../frameworks/base
-cp $LOCALDIR/patches/system_core.patch ../../../system/core
+cp $LOCALDIR/patches/build.patch ../../../../build
+cp $LOCALDIR/patches/external_tinyalsa.patch ../../../../external/tinyalsa
+cp $LOCALDIR/patches/frameworks_av.patch ../../../../frameworks/av
+cp $LOCALDIR/patches/frameworks_base.patch ../../../../frameworks/base
+cp $LOCALDIR/patches/system_core.patch ../../../../system/core
 
 # Apply them
-cd ../../..
+cd ../../../..
 cd build && patch -p1 < *.patch
 cd ../external/tinyalsa && patch -p1 < *.patch
 cd ../../frameworks/av && patch -p1 < *.patch
@@ -18,4 +18,4 @@ cd ../../system/core && patch -p1 < *.patch
 
 #Move XML to .repo folder
 
-cp $LOCALDIR/XML/grandprimeve3g.xml ../../../.repo/manifests
+cp $LOCALDIR/XML/grandprimeve3g.xml ../../../../.repo/manifests
