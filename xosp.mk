3255132d469ed5e8b1c1331ac4e29ebbaf96f916
Remilia <remilia.1505@gmail.com>
2017-01-07 14:10:11 +0700

    xosp-n: Update configs for XOSP Nougat

--- xosp.mk
+++ xosp.mk
@@ -1,18 +1,19 @@
 ## Specify phone tech before including full_phone	
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/xosp/config/telephony.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := grandprimeve3g
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common xosp stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device_grandprimeve3g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grandprimeve3g
-PRODUCT_NAME := cm_grandprimeve3g
+PRODUCT_NAME := xosp_grandprimeve3g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G531H
 PRODUCT_MANUFACTURER := samsung
