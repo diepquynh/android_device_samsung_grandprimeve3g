c75acf3e05e07e375917fc8f9dd51cc17df22027
Remilia Scarlet <remilia.1505@gmail.com>
2017-02-09 17:10:28 +0700

    cm: Rebrand to Lineage
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -5,14 +5,14 @@ $(call inherit-product, vendor/cm/config/telephony.mk)
 PRODUCT_RELEASE_NAME := grandprimeve3g
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device_grandprimeve3g.mk)
+$(call inherit-product, $(LOCAL_PATH)/grandprimeve3g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grandprimeve3g
-PRODUCT_NAME := cm_grandprimeve3g
+PRODUCT_NAME := lineage_grandprimeve3g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G531H
 PRODUCT_MANUFACTURER := samsung
