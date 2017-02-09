fa5b84e9c540cfd051e82b5c60796fc07e2d8e08
Remilia Scarlet <remilia.1505@gmail.com>
2017-02-09 17:06:32 +0700

    BoardConfig: Move to common
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,40 +12,107 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from SPRD common configs
--include device/samsung/sprd-common/BoardConfigCommon.mk
+# Inherit from scx30g_v2 common configs
+-include device/samsung/scx30g_v2-common/BoardConfigCommon.mk
 
 # Inherit from the proprietary version
 -include vendor/samsung/grandprimeve3g/BoardConfigVendor.mk
 
-# Platform
-TARGET_ARCH := arm
-TARGET_BOARD_PLATFORM := sc8830
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := SC7730SE
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
-BOARD_VENDOR := samsung
-TARGET_UNIFIED_DEVICE := true
+# Camera
+#zsl capture
+TARGET_BOARD_CAMERA_CAPTURE_MODE := false
 
-# Config u-boot
-TARGET_NO_BOOTLOADER := true
+#back camera rotation capture
+TARGET_BOARD_BACK_CAMERA_ROTATION := false
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5872025600
-BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_PLATFORM_DEVICE_BASE := /devices/sdio_emmc/
+#front camera rotation capture
+TARGET_BOARD_FRONT_CAMERA_ROTATION := false
+
+#rotation capture
+TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
+
+#support hal1.0,hal3.2
+TARGET_BOARD_CAMERA_HAL_VERSION := 1.0
+
+# camera sensor type
+CAMERA_SENSOR_TYPE_BACK := "s5k4h5yc_mipi"
+CAMERA_SENSOR_TYPE_FRONT := "s5k5e3yx_mipi"
+
+# select camera 2M,3M,5M,8M
+CAMERA_SUPPORT_SIZE := 8M
+FRONT_CAMERA_SUPPORT_SIZE := 5M
+TARGET_BOARD_NO_FRONT_SENSOR := false
+TARGET_BOARD_CAMERA_FLASH_CTRL := false
+
+#read sensor otp to isp
+TARGET_BOARD_CAMERA_READOTP_TO_ISP := true
+
+# use sprd auto lens
+TARGET_BOARD_CAMERA_SPRD_AUTOLENS := false
+
+#otp version, v0(OTP on Grandprime, Z3) v1(OTP on J1MINI) v2(Without OTP on TabG)
+TARGET_BOARD_CAMERA_OTP_VERSION := 0
+
+#read otp method 1:from kernel 0:from user
+TARGET_BOARD_CAMERA_READOTP_METHOD := 1
+
+#face detect
+TARGET_BOARD_CAMERA_FACE_DETECT := true
+TARGET_BOARD_CAMERA_FD_LIB := omron
+
+#sensor interface
+TARGET_BOARD_BACK_CAMERA_INTERFACE := mipi
+TARGET_BOARD_FRONT_CAMERA_INTERFACE := mipi
+
+#select camera zsl cap mode
+TARGET_BOARD_CAMERA_CAPTURE_MODE := true
+
+#select camera zsl force cap mode
+TARGET_BOARD_CAMERA_FORCE_ZSL_MODE := true
+
+#rotation capture
+TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
+
+#select camera not support autofocus
+TARGET_BOARD_CAMERA_NO_AUTOFOCUS_DEV := false
+
+#uv denoise enable
+TARGET_BOARD_CAMERA_CAPTURE_DENOISE := false
+
+#y denoise enable
+TARGET_BOARD_CAMERA_Y_DENOISE := true
+
+#select continuous auto focus
+TARGET_BOARD_CAMERA_CAF := true
+
+#select ACuteLogic awb algorithm
+TARGET_BOARD_USE_ALC_AWB := true
+
+#pre_allocate capture memory
+TARGET_BOARD_CAMERA_PRE_ALLOC_CAPTURE_MEM := true
+
+#sc8830g isp ver 0;sc9630 isp ver 1;tshark2 isp version 2
+TARGET_BOARD_CAMERA_ISP_SOFTWARE_VERSION := 2
+
+#support auto anti-flicker
+TARGET_BOARD_CAMERA_ANTI_FLICKER := true
+
+#multi cap memory mode
+TARGET_BOARD_MULTI_CAP_MEM := true
+
+#select mipi d-phy mode(none, phya, phyb, phyab)
+TARGET_BOARD_FRONT_CAMERA_MIPI := phyc
+TARGET_BOARD_BACK_CAMERA_MIPI := phyab
+
+#select ccir pclk src(source0, source1)
+TARGET_BOARD_FRONT_CAMERA_CCIR_PCLK := source0
+TARGET_BOARD_BACK_CAMERA_CCIR_PCLK := source0
+
+#hdr effect enable
+TARGET_BOARD_CAMERA_HDR_CAPTURE := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/grandprimeve3g/bluetooth
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
@@ -54,102 +121,9 @@ TARGET_KERNEL_CONFIG := cyanogen_grandprimeve3g_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/grandprimeve3g
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/grandprimeve3g/dt.img
 
-# RIL
-BOARD_RIL_CLASS += ../../../device/samsung/grandprimeve3g/ril
-COMMON_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
-
-# FM radio
-BOARD_HAVE_FM_BCM := true
-BOARD_HAVE_FMRADIO_BCM := true
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/grandprimeve3g/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/grandprimeve3g/bluetooth/libbt_vndcfg.txt
-
-# Wifi
-BOARD_WLAN_DEVICE := bcmdhd
-BOARD_WLAN_DEVICE_REV := bcm4343
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_NVRAM_PATH_PARAM := "/sys/module/dhd/parameters/nvram_path"
-WIFI_DRIVER_NVRAM_PATH := "/system/etc/wifi/nvram_net.txt"
-WIFI_BAND := 802_11_ABG
-BOARD_HAVE_SAMSUNG_WIFI := true
-
-# Graphics
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-BOARD_EGL_NEEDS_HANDLE_VALUE := true
-HWUI_COMPILE_FOR_PERF := true
-TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
-COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-
-# HWComposer
-USE_SPRD_HWCOMPOSER := true
-USE_SPRD_DITHER := true
-USE_OVERLAY_COMPOSER_GPU := true
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-
-# Enable WEBGL in WebKit
-ENABLE_WEBGL := true
-
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# Lights
-TARGET_HAS_BACKLIT_KEYS := false
-
 # Resolution
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
-# Codecs
-COMMON_GLOBAL_CFLAGS += -DBOARD_CANT_REALLOCATE_OMX_BUFFERS
-SOC_SCX30G_V2 := true
-
-# Board specific features
-BOARD_USE_SAMSUNG_COLORFORMAT := true
-
-# healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.sc8830
-
-# Init
-TARGET_NR_SVC_SUPP_GIDS := 36
-
-# Recovery
-BOARD_HAS_DOWNLOAD_MODE := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_SUPPRESS_EMMC_WIPE := true
-TARGET_RECOVERY_FSTAB := device/samsung/grandprimeve3g/rootdir/fstab.sc8830
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SM-G531H,SM-G531BT,grandprimeve3g,grandprimeve3gdtv,grandprimeve3gub,grandprimeve3gxx,grandprimeve3gdtvvj
-
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_sec
-
-# PowerHAL
-TARGET_POWERHAL_VARIANT := samsung
-
-# Use dmalloc() for such low memory devices like us
-BOARD_USES_LEGACY_MMAP := true
-
-# Enable dex-preoptimization to speed up the first boot sequence
-WITH_DEXPREOPT := true
-WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-WITH_DEXPREOPT_PIC := true
-WITH_DEXPREOPT_COMP := false
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_NO_CHARGER_LED := true
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
-BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
-
