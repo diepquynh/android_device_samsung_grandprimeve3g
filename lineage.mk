7b6cb6aac915a798eec8f6dac83baed344ac9dfd
Remilia Scarlet <remilia.1505@gmail.com>
2017-04-23 12:30:12 +0700

    device: Move build fingerprint to Lineage makefile
    This is the correct one
    
    Change-Id: I8c21413f331835cb545b4922080e79431609e164
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -10,6 +10,11 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/grandprimeve3g.mk)
 
+# Stock build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=samsung/grandprimeve3gxx/grandprimeve3g:5.1.1/LMY48B/G531HDDU0APK2:user/relea$
+    PRIVATE_BUILD_DESC="grandprimeve3gxx-user 5.1.1 LMY48B G531HDDU0APK2 release-keys"
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grandprimeve3g
 PRODUCT_NAME := lineage_grandprimeve3g
