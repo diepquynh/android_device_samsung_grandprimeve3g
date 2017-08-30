dc603ce329a9334e4889ab5920e9122aaecb5202
Juan Cortes <fam.cortesgutierrez@hotmail.com>
2017-08-30 00:34:51 -0300

    Deleted CM Stuff and Added right route to AOSP stuff

--- aosp_grandprimeve3g.mk
+++ aosp_grandprimeve3g.mk
@@ -1,11 +1,8 @@
 ## Specify phone tech before including full_phone	
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, /build/target/product/aosp_base_telephony.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := grandprimeve3g
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/grandprimeve3g.mk)
