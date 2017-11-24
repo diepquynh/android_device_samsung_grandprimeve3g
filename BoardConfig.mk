djeman/android_device_samsung_j3xnlte

ril: override ril.h
Change-Id: Ie7de456d44b3f84ac82e1c2c5b7fa8c349895bb0

BoardConfig.mk
@@ -27,7 +27,7 @@ DEVICE_RESOLUTION := 720x1280
 TARGET_BOOTLOADER_BOARD_NAME := SC9830I
 
 # Include path
-#TARGET_SPECIFIC_HEADER_PATH := device/samsung/j3xnlte/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/j3xnlte/include
 
 #TARGET_PREBUILT_KERNEL := device/samsung/j3xnlte/kernel
 #TARGET_PREBUILT_DTB := device/samsung/j3xnlte/dt.img

diepquynh/android_device_samsung_grandprimeve3g

Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

BoardConfig.mk
@@ -159,3 +159,6 @@ TARGET_SCREEN_WIDTH := 540
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SM-G531H,SM-G531BT,grandprimeve3g,grandprimeve3gdtv,grandprimeve3gub,grandprimeve3gxx,grandprimeve3gdtvvj
+
+# RIL
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/grandprimeve3g/include